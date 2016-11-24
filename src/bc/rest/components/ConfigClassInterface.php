b37b618ee2bab746506adcbc0616b48e8f429068
theinpu <theinpu@gmail.com>
2016-11-24 16:07:24 +0300

    fill config interface

--- src/bc/rest/components/ConfigClassInterface.php
+++ src/bc/rest/components/ConfigClassInterface.php
@@ -3,7 +3,44 @@
 
 namespace bc\rest\components;
 
-
+/**
+ * Interface ConfigClassInterface
+ * Represents config file
+ *
+ * @package bc\rest\components
+ */
 interface ConfigClassInterface extends ComponentInterface {
+
+    /**
+     * Set config option value
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    public function setOption($name, $value);
+
+    /**
+     * Get config option by name
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function getOption($name);
+
+    /**
+     * Get all options
+     *
+     * @return mixed[]
+     */
+    public function getOptions();
+
+    /**
+     * Remove named config option
+     *
+     * @param string $name
+     *
+     */
+    public function removeOption($name);
 
 }
